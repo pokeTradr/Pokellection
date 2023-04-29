909b23f19529f931994181f53a47311dda710e42
Ijoo <ijooyoon.97@gmail.com>
2023-04-29 12:34:01 -0400

    tried first middleware to fetch from API, error in node fetch

---
+++ server/controller/APIController.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+
+const APIController = {};
+
+APIController.call = (req, res, next) => {
+  fetch('https://api.pokemontcg.io/v2/cards/')
+  .then(data => data.json())
+  .then(data => {
+    res.locals.pokemonData = data;
+    return next();
+  })
+  .catch((err) => {
+    const errorObj = {
+      log: 'Pokemon data not fetched properly',
+      status: 400,
+      message: 'Uh oh! Your pokemon data was not fetched properly'
+    }
+    return next(errorObj);
+  })
+}
+
+APIController.instantiateTable = (req, res, next) => {
+
+}
+
+
+
+module.exports = APIController;
+
--- server/server.js
+++ server/server.js
@@ -1,16 +1,41 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
+const APIController = require('./controller/APIController')
 const app = express();
 const PORT = 3000;
 
 // app.use('/', express.static(path.join(__dirname,'')))
 
-app.get('/hello', (req, res) => {
-  console.log('made a request');
-  res.status(200).send('hello I am a response');
+app.get(
+  "/",
+  APIController.call,
+  (req, res) => {
+    return res.status(200).json(res.locals.pokemonData)
+  }
+);
+
+app.get("/hello", (req, res) => {
+  console.log("made a request");
+  res.status(200).send("hello I am a response");
+});
+
+
+
+app.use('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error global occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => {
-  console.log('listening on a port:', PORT);
+  console.log("listening on a port:", PORT);
 });
