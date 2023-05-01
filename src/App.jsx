bd909a6a416201d4a498aeccb73f57dfed2bb829
kennyyang2015 <kennyyang2015@gmail.com>
2023-05-01 17:43:40 -0400

    finished frontend on login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,68 @@
-import React from "react";
+import React, {useEffect, useState, useRef} from 'react';
 import MainContainer from './client/containers/mainContainer';
+import Login from './client/containers/login';
 import './styles.css';
 
-export default function App() {
+export default function App(props) {
+    const newUsername = useRef(null)
+    const newPassword = useRef(null)
+
+
+    const username = useRef(null)
+    const password = useRef(null)
+    const [isVisible, setIsVisible] = useState(false);
+    function storeUser(){
+        console.log(newUsername.current.value)
+        console.log(newPassword.current.value)
+        fetch('/signup', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: newUsername.current.value,
+                password: newPassword.current.value
+            })
+        })
+        .then(res => console.log("sucessful adding the user"))
+        .catch(err => console.log(err))
+    }
+
+    function showComponent(event) {
+        console.log(username.current.value)
+        console.log(password.current.value)
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username.current.value,
+                password: password.current.value
+            })
+        })
+        // .then(res => setIsVisible(res.json()))
+        // .catch(err => console.log(err))
+        setIsVisible(!isVisible);
+    }
     return (
         <div id="App">
-            <MainContainer />
+
+            <div id="signup">
+                <input placeholder="Create a Username" ref={newUsername}/>
+                <input placeholder="Create a Password" ref={newPassword}/>
+                <button onClick={storeUser}>Signup</button>
+            </div>
+            <div>
+                <input placeholder="Username" ref={username}/>
+                <input placeholder="Password" ref={password}/>
+                <button onClick={showComponent}>Login</button>
+            </div>
+            {
+                isVisible ? <MainContainer/> : null
+            }
         </div>
+
     )
 }
 
---
+++ src/client/containers/login.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+export default function Login(){
+  return
+}
