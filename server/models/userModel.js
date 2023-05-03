2a9b85980e1a7a46aefea342383672d595f14452
Wayland Singh <wmsingh@ucdavis.edu>
2023-05-03 13:46:23 -0400

    syntax, comments added removed unneeded test file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('dotenv').config();
 
-// const MONGO_URI: 'mongodb+srv://yangyohan1:9Ev7mpUpSgDm6K78@cluster0.kiof2xb.mongodb.net/?retryWrites=true&w=majority';
-// console.log('mongoURI:', process.env.mongoURI);
-// REENABLE AFTER TESTS? find a way to do this when its NOT testing
+// for sswitching between test/dev
 // console.log(process.env);
 if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(process.env.mongoURI, {
@@ -22,7 +20,7 @@ const bcrypt = require('bcryptjs');
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  deckList: { type: Array }
+  deckList: { type: Array },
 });
 
 module.exports = mongoose.model('User', userSchema);
