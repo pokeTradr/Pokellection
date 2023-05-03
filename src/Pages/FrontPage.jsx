f4e170b262280d7ff7b50c365bc72b2fab2a2b3d
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-05-03 12:02:10 -0400

    front page styling

--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import '../styles.css'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles.css";
 
 // Front page that will allow users to either sign up or log in
 function FrontPage() {
   const navigate = useNavigate();
 
   return (
-    <div className='front-page-container'>
-        <h1>Pokéllection</h1>
-        <div className='buttons-container'>
-            <button className='signup_btn' onClick={() => navigate('./Signup')}>Sign Up</button>
-            <button className='login_btn' onClick={() => navigate('./Login')}>Login</button>
-        </div>
-
+    <div className="front-page-container">
+      <h1>PoKéllectioN</h1>
+      <div className="buttons-container">
+        <button className="signup_btn" onClick={() => navigate("./Signup")}>
+          Sign Up
+        </button>
+        <button className="login_btn" onClick={() => navigate("./Login")}>
+          Login
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FrontPage
+export default FrontPage;
