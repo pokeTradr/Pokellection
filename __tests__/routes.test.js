ee1282482f54e019011752544a8fbc284c26369c
Wayland Singh <wmsingh@ucdavis.edu>
2023-05-03 12:06:26 -0400

    dependencies for supertest

---
+++ __tests__/routes.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./../server/server');
+
+// after all, shut down the server
+describe('test express routes', () => {
+  test('signup and login routes', () => {
+    expect(false).toEqual(true);
+  });
+});
