2900a2cfc6bb0b0c0f787f1069ae39078cec6dd5
Wayland Singh <wmsingh@ucdavis.edu>
2023-05-04 14:14:28 -0400

    icons added

---
+++ src/assets/type-icons/typeIcons.js
@@ -0,0 +1,18 @@
+import Dark from './Dark.png';
+import Electric from './Electric.png';
+import Fairy from './Fairy.png';
+import Fighting from './Fighting.png';
+import Ground from './Ground.png';
+import Ice from './Ice.png';
+import Normal from './Normal.png';
+import Poison from './Poison.png';
+import Psychic from './Psychic.png';
+import Rock from './Rock.png';
+import Steel from './Steel.png';
+import Water from './Water.png';
+
+export default {
+  Dark: Dark,
+  Electric: Electric,
+  Fairy: Fairy,
+};
