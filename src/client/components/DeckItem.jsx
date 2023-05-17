e93760119ad91b1edbe71e5083520bcba0d08bca
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2023-05-17 15:01:08 -0400

    added functionality to delete individual cards from the deck

--- src/client/components/DeckItem.jsx
+++ src/client/components/DeckItem.jsx
@@ -4,7 +4,7 @@ function DeckItem(props) {
     const [isHoveredOver, setIsHoveredOver] = useState(null)
 
     const onHoverHandler = (index) => {
-      console.log(props.listLength)
+      console.log(index)
       console.log('in the OnHoverHandler')
       setIsHoveredOver(index)
     }
@@ -14,19 +14,23 @@ function DeckItem(props) {
       setIsHoveredOver(null)
     }
 
+
   return (
     <div 
     className='card'
     key = {props.index}
     style = {{
+      cursor: 'pointer',
       position: 'absolute',
-      left : props.index === 1 ? `${props.index * 20}px` : `${props.index * 50}px`,
+      left : `${props.index * 50}px`,
       zIndex : isHoveredOver === props.index ? props.listLength + 1: props.index,
       transform : isHoveredOver === props.index ? 'scale(1.1)' : 'scale(1)',
       transition : 'transform .4s ease-out'
     }}
     onMouseOver = {() => onHoverHandler(props.index)}
     onMouseLeave = {() => notOnHoverHandler(props.index)}
+    onClick = {props.onClick}
+  
     >
         {props.pokemon.images.large ? 
           <img className = 'card' src={props.pokemon.images.large}/>
--- src/client/components/List.jsx
+++ src/client/components/List.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import DeckItem from './DeckItem';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCard } from "../../redux/DeckList";
 
 function List() {
+  const dispatch = useDispatch()
   const { list } = useSelector((state) => state.deckList);
+
+
+  const clickHandler = (index) => {
+    dispatch(removeCard(index));
+  };
 
   return (
     <div className='collection'>
       {console.log('list: ', list)};{console.log('list[0]: ', list[0])};
       {list.map((el, index) => (
-        <DeckItem pokemon={el} index={index + 1} listLength={list.length} />
+        <DeckItem pokemon={el} index={index + 1} listLength={list.length} onClick = {() => clickHandler(index)}/>
       ))}
     </div>
   );
